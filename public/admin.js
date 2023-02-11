6c1b5c1ecb4bae05b717a1e425849ae77a75f8da
Dimario Oliver <Dimario Oliver>
2023-02-10 22:16:56 -0500

    done

--- public/admin.js
+++ public/admin.js
@@ -8,4 +8,34 @@ async function listBooks(){
     
     books.forEach(newbook)
     }
+    function newbook(book){
     
+        let root = document.querySelector('#root')
+        
+        let list = document.createElement('li')
+        list.textContent = book.title 
+        
+        let quantityretr = document.createElement('input')
+        quantityretr.value = book.quantity
+        
+        let save = document.createElement('button')
+        save.textContent = 'Save'
+        
+        save.addEventListener('click', () => {
+            fetch('http://localhost:3001/updateBook', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: book.id,
+                    quantity: quantityretr.value
+                })
+            })
+        })
+        list.append(quantityretr,save)
+        root.append(list)
+        
+        }
+        
+        listBooks()
