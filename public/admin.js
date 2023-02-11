a6ad0e989254ff6e77c4adcd5670d1e194a04cfe
Dimario Oliver <Dimario Oliver>
2023-02-10 22:15:56 -0500

    retrieve books

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,11 @@
+async function listBooks(){
 
-// Your Code Here
+    let response = await fetch ('http://localhost:3001/listBooks')
+    
+    
+    
+    let books = await response.json()
+    
+    books.forEach(newbook)
+    }
+    
